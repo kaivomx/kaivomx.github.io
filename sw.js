bd23b6e59eb27e2cf16ae1a985c05b1e2d1163e0
Test <test@mac-edu.local>
2026-07-01 22:14:37 -0600

    Forzar actualizacion cache v3

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='kaivo-v1';
+const CACHE='kaivo-v3';
 const FILES=['/crm.html','/index.html','/manifest.json'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
