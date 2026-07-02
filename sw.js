72ef891621b33d44b562eb746419159e54e6b5f3
Test <test@mac-edu.local>
2026-07-01 21:41:23 -0600

    PWA: app instalable en celular

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE='kaivo-v1';
+const FILES=['/crm.html','/index.html','/manifest.json'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
